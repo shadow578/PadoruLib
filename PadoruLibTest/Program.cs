f55c02c9b70040fb946a2d8f6f61ba7163c60e81
shadow578 <christophnienhaus2@gmail.com>
2019-11-19 17:43:45 +0100

    Add Test Project to test the Library, remove old Test class

---
+++ PadoruLibTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using PadoruLib.Padoru;
+using PadoruLib.Padoru.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadoruLibTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Padoru();
+        }
+
+        static async void Padoru()
+        {
+            //init client
+            PadoruClient client = new PadoruClient();
+            client.MaxCollectionAge = TimeSpan.FromSeconds(10);//for testing auto resync
+            await client.LoadCollection(@"https://raw.githubusercontent.com/shadow578/Padoru-Padoru/master/padoru.json");
+
+            //test get all entries
+            IReadOnlyCollection<PadoruEntry> allEntries = await client.GetEntries();
+
+            //force resync of collection
+            await Task.Delay(15);
+
+            //test get random entry (with resync)
+            PadoruEntry randomEntry = await client.GetRandomEntry();
+            client.MaxCollectionAge = TimeSpan.FromHours(1);
+
+            //test get specific entry
+            IReadOnlyCollection<PadoruEntry> maleEntries = await client.GetEntriesWhere((p) => !p.IsFemale);
+
+            //test image download
+            Image rndImg = await randomEntry.GetImage();
+            rndImg?.Save("./random.png");
+        }
+    }
+}
